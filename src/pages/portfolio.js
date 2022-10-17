d326b1664d71adfdfe1f88346fd335717098301a
ClaytonDewey <clay@dryadmedia.com>
2022-10-17 13:22:56 -0500

    Setup base portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Loader from "react-loaders";
 
 const Portfolio = () => {
   return (
-    <div>
-      <h1>portfolio</h1>
-    </div>
+    <>
+      <section className="portfolio__section">
+        <h1 className="section__title">
+          Portfolio
+          <br />
+          <span>My Work</span>
+        </h1>
+      </section>
+      <Loader type="pacman" />
+    </>
   );
 };
 
