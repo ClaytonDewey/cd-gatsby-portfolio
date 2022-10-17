13e5c951c7cee4c81384bf0e4b4b024cb3c4e096
ClaytonDewey <clay@dryadmedia.com>
2022-10-17 09:03:50 -0500

    Setup homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { FaJediOrder } from "react-icons/fa";
 
 const IndexPage = () => {
   return (
-    <main>
-      <section>
-        <h1>Hello...</h1>
+    <>
+      <section className="home__section">
+        <div className="home__section-wrapper">
+          <div className="home__section-info">
+            <StaticImage src="../assets/images/profile.jpeg" alt="avatar" />
+            <h1>Clayton Dewey</h1>
+            <p className="subhead">Front End Developer</p>
+            <p className="text-center">
+              <FaJediOrder />
+            </p>
+          </div>
+        </div>
       </section>
-    </main>
+    </>
   );
 };
 
